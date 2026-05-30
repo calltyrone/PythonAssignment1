00e94d9ea88a15b2b09ae9babc617fd8be98f425
calltyrone <90288373+calltyrone@users.noreply.github.com>
2026-05-30 00:30:10 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -40,11 +40,11 @@ def welcome_assignment_answers(question):
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 
-        answer = "4"
+        answer = "1"
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
 
-        answer = "2"
+        answer = "3"
 
     else:
 
